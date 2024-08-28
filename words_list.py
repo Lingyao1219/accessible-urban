ee6dc6b914e3c3ef36f1750f4e003368ffd5f60b
Lingyao <125314658+Lingyao1219@users.noreply.github.com>
2024-08-27 22:08:11 -0400

    Add word_list

---
+++ words_list.py
@@ -0,0 +1,41 @@
+include_list = [
+    "accessible",
+    "accessibility",
+    "ada compliance",
+    "ada compliant",
+    "blind",
+    "braille",
+    "curb cut",
+    "curb ramp",
+    "deaf",
+    "disabilities",
+    "disability",
+    "disabled",
+    "grab rail",
+    "hand splint",
+    "handicap",
+    "handrail",
+    "hearing loss"
+    "induction loop",
+    "mobility aid",
+    "service animal",
+    "service cat",
+    "service dog",
+    "tactile map",
+    "tactile paving",
+    "tactile warning",
+    "tgsi",
+    "vision impairment",
+    "visual impairment",
+    "wheelchair"
+]
+
+
+exclude_list = [
+    'accessible place',
+    'accessible price',
+    'blindly',
+    'blinds'
+    'blindspot',
+    'blind spot'
+]
