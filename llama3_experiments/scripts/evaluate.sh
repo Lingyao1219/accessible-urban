a20759183fc56ff509ff941f7758f24b9c6502d4
Yinpei Dai <daiyp@sled-aspen.eecs.umich.edu>
2024-09-10 18:15:55 -0400

    fix

--- llama3_experiments/scripts/evaluate.sh
+++ llama3_experiments/scripts/evaluate.sh
@@ -1,3 +1,4 @@
+# Evaluate on one GPU (at least 18GB GPU memory)
 MODEL_NAME=accessibility_llama3-8b-lora32_alpha16_ep10_bs64_lr3e-5 # our best model checkpoint
 MODEL_BASE=<Path_to_your_downloaded_Meta-Llama-3-8B-Instruct-HF> # downloaded from Hugging Face
 CUDA_VISIBLE_DEVICES=1 python eval.py --model-path checkpoints/${MODEL_NAME} --model-base ${MODEL_BASE} --test-files ../test.jsonl  --key target_text
